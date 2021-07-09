d72e5386d97535a09a6544998b0b3eef8996a6d9
Sayan-Ghosh <ghosh.sayan999@gmail.com>
2021-07-10 00:18:06 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const app = express();
+
+const convertUrl = (url) => {
+		
+	let newUrlArray;
+
+	if(url.includes("youtu.be")){
+
+	 newUrlArray = url.split("https://youtu.be/")
+	return `https://www.youtube.com/watch?v=${newUrlArray[1]}`
+
+	}else
+	 if(url.includes("https://www.youtube.com/shorts/")){
+
+		 newUrlArray = url.split("https://www.youtube.com/shorts/")
+		return `https://www.youtube.com/watch?v=${newUrlArray[1]}`
+	}
+	else if(url.includes("https://www.youtube.com/watch?v")){
+		return url
+	}
+	else{
+		return "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+	}
+}
+
+// video
+app.get("/video", async(req,res)=>{
+	const videoId = convertUrl(req.query.videoId,res)
+	let info = await ytdl.getInfo(videoId)
+	res.json(info)
+})
+
+// audio
+app.get("/audio", async(req,res)=>{
+	const videoId = convertUrl(req.query.videoId)
+	let info = await ytdl.getInfo(videoId)
+	let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+	res.json(audioFormats)
+})
+
+// Download video with custom title and quality
+app.get("/download", async(req,res)=>{
+
+	const {itag,title,type} = req.query
+	const videoId = convertUrl(req.query.videoId)
+
+	if(type === "mp4"){
+		res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
+		res.header('Content-Type', 'video/mp4');
+	}else if(type === "mp3"){
+		res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
+		res.header('Content-Type', 'audio/mp3');
+	}
+
+	ytdl(videoId, { filter: format => format.itag === 18 }).pipe(res); 
+	
+})
+
+
+app.listen(4000,console.log("working..."))
